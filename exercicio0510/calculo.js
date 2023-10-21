6d8a554c1023467a2dc540723f147c04bdbd8d7d
nandaBe <130059223+nandadosoob@users.noreply.github.com>
2023-10-21 15:14:08 -0300

    exerciciorepeticao

--- exercicio0510/calculo.js
+++ exercicio0510/calculo.js
@@ -1,16 +1,54 @@
-function ex01() {
 
+function ex01() {
+    let num = document.getElementById("input_numero").value;
+    let resultado = document.getElementById("resultado");
+
+    resultado.innerHTML = ""; // Limpa o conteúdo anterior
+
+    for (let i = 1; i <= 10; i++) {
+        let resul = num * i;
+        resultado.innerHTML += `${num}x${i} = ${resul}<br>`;
+        }
 }
 
+
+
 function ex02() {
+    let num = document.getElementById("input_numero").value;
+    let inicio = document.getElementsById("input_comeco").value;
+    let fim = document.getElementById("input_final").value;
+    let resultado = document.getElementById("resultado");
+
+    resultado.innerHTML = "";
+
+    if (inicio <= fim) {
+        for (let i = inicio; i <= fim; i++) {
+            let resul = num * i;
+            resultado.innerHTML += `${num}x${i} = ${resul}<br>`;
+        }
+    } else {
+        resultado.innerHTML = "O início deve ser menor ou igual ao fim.";
+    }
 
 }
 
 
 
 function ex03() {
+    let num = (document.getElementById("input_numero").value);
+        let resultado = document.getElementById("resultado");
 
-}
+        resultado.innerHTML = ""; // Limpa o conteúdo anterior
+
+        let fib = [1, 1];
+
+        for (let i = 2; i < num; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        resultado.innerHTML = "Sequência de Fibonacci: " + fib.join(", ");
+        }
+
 
 
 
@@ -21,6 +59,8 @@ function ex04() {
 
 
 function ex05() {
+
+    
 
 }
 
