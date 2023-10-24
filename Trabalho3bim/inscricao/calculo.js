bf4eb22794c6901044ed1cab5d3e3f325a75a38f
nandaBe <130059223+nandadosoob@users.noreply.github.com>
2023-10-23 21:35:33 -0300

    a

--- Trabalho3bim/inscricao/calculo.js
+++ Trabalho3bim/inscricao/calculo.js
@@ -1,24 +1,45 @@
 let nome = document.getElementById("input_nome").value
 let email = document.getElementById("input_email").value
 let senha = document.getElementById("input_senha").value
-let erro_senha = document.getElementById("erro_senha").value
-let erro_campo = document.getElementById("erro_campo").value
+//let erro_senha = document.getElementById("erro_senha").value
+//let erro_campo = document.getElementById("erro_campo").value
 
-function qnt_senha() {
-    if (senha.length < 8 ) {
-        erro_senha.textContent = "A senha precisa ter no mínimo 8 caracteres"
+function validar() { // TIRAR DUVIDA COM O PROFESSOR SOBRE PQ N DA CERTO SENDO SÓ "nome"
+    if (input_nome == "" && input_email == "" && input_senha == "") { 
+        alert("Todos os campos precisam ser preenchidos");
+    }
+
+    else if (input_senha.length < 8) {
+        alert("Sua senha deve conter no mínimo 8 caracteres");
     }
 
     else {
-        erro_senha = "";
-    }}
+        alert("Cadastro realizado");
+    }
+    //if (senha.lenght < 8) {
+      //  alert("Sua senha deve conter no mínimo 8 caracteres");
+    //}
 
-function campos_preenchidos() { 
-    if (nome.length < 1 && email.length <1 && senha.length < 1){
-        erro_campo = "Todos os campos devem ser preenchidos"
-    }
-    else {
-        erro_campo = "";
-    }
+
+
 }
+
+
+//function qnt_senha() {
+//    if (senha.length < 8 ) {
+//        erro_senha.textContent = "A senha precisa ter no mínimo 8 caracteres"
+//    }
+
+//    else {
+//        erro_senha = "";
+//    }}
+
+//function campos_preenchidos() { 
+//    if (nome.length < 1 && email.length <1 && senha.length < 1){
+//        erro_campo = "Todos os campos devem ser preenchidos"
+//    }
+//    else {
+//        erro_campo = "";
+//    }
+//}
 
