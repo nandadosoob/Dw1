b706e12644eb8db56c558d6d4dd542eac060d6c0
nandaBe <130059223+nandadosoob@users.noreply.github.com>
2023-11-01 13:47:10 -0300

    nao ta funfando

--- Ex_calc_gorgeta/arquivos/scripts.js
+++ Ex_calc_gorgeta/arquivos/scripts.js
@@ -1,14 +1,15 @@
 function calculo_gorjeta() {
     let val_conta = document.getElementById("valorConta").value;
     let porcentagem = document.getElementById("qualidadeServico").value;
-    let resultado_gorjeta = document.getElementsByClassName("resultado");
-    let resultado_total = document.getElementsByClassName("valorTotal");
-
+    
     if (val_conta === "" || porcentagem === "") {
         alert("Todos os campos devem ser preenchidos");
     }else{
-        resultado_gorjeta.innerHTML =  (porcentagem * val_conta);
-        resultado_total.innerHTML = (val_conta + resultado_gorjeta);
+        let resultado_gorjeta = document.getElementsById("tipoConta");
+        let resultado_total = document.getElementsById("valorTotal");
+
+        resultado_gorjeta.innerHTML =  (porcentagem * val_conta)
+        resultado_total.innerHTML = (val_conta + resultado_gorjeta)
     }
 
 }
