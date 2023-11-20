8688c22c932a6ee8078edb79f143c27fa8232d79
nandaBe <130059223+nandadosoob@users.noreply.github.com>
2023-11-20 18:36:07 -0300

    aaaaaaaaa

---
+++ codigo_aula/script.js
@@ -0,0 +1,7 @@
+function puxar(){
+    let mob = document.querySelector(".menu-mobile")
+    mob.style.right="0";
+
+    //let mob2 = document.querySelector(".menu-mobile")
+    //mob2.style.right="100%";
+}
