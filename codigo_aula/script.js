161b74880a48c599671d6a9d01d1b56f70034126
nandaBe <130059223+nandadosoob@users.noreply.github.com>
2023-12-02 15:20:39 -0300

    terminei deus é bom

--- codigo_aula/script.js
+++ codigo_aula/script.js
@@ -1,16 +1,14 @@
 let menuMobile = document.querySelector(".menu-mobile")
-let menuIcon = document.querySelector(".menuIcon img")
+let menuIcon = document.querySelector("#imgMenu")
 
 function puxar(){
-    //if (menuMobile.style.right === "100px"){
-
-        //menuIcon.getAttribute("src") === "icones/xmark-solid.svg"
-        //menuIcon.setAttribute("src", "icones/bars-solid.svg")
-        menuMobile.style.right="0";
-   // } 
-    //else if(menuIcon.getAttribute("src") === "icones/bars-solid.svg"){
-        //menuIcon.setAttribute("src", "icones/bars-solid.svg")
-     //   menuMobile.style.right="100%";
-    //}
-
+    if (menuMobile.style.right === '100%'){
+        menuMobile.style.right = '0%'
+        menuIcon.src = "./imagens/xmark-solid.svg"
+    }
+    
+    else{
+        menuMobile.style.right = '100%'
+        menuIcon.src = "./imagens/bars-solid.svg"
+    }
 }
