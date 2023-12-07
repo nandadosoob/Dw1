f23c77c9ab2303a248a4b866fd8a84f17f3334d7
nandaBe <130059223+nandadosoob@users.noreply.github.com>
2023-12-06 21:20:47 -0300

    to cansada

---
+++ trabalhoDW4/mostra_filmes/script.js
@@ -0,0 +1,13 @@
+let mobile = document.querySelector("#menu-mobile")
+let icone = document.querySelector("#icon")
+function menu(){
+    if (mobile.style.right === '100%'){
+        mobile.style.right = '0%'
+        icone.src = "./icones/xmark-solid.svg"
+    }
+
+    else{
+        mobile.style.right = '100%'
+        icone.src = "./icones/bars-solid.svg"
+    }
+}
