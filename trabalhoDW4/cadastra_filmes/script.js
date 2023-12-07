675cce9914104058a1c3f7ac4ef5947c462e83eb
nandaBe <130059223+nandadosoob@users.noreply.github.com>
2023-12-07 09:58:53 -0300

    socorro é mt coisa

--- trabalhoDW4/cadastra_filmes/script.js
+++ trabalhoDW4/cadastra_filmes/script.js
@@ -1,23 +1,22 @@
 function validar() {
-    let titulo = document.querySelector("#titulo").value
-    let cod_filme = document.querySelector("#cod_filme").value
-    let sinopse = document.querySelector("#sinopse").value
-    let datalanc = document.querySelector("#datalanc").value
-    let classificacao = document.querySelector("#classificacao").value
-    let duracao = document.querySelector("#duracao").value
-    
-    if (titulo ===""  || cod_filme === "" || sinopse === "" || datalanc === "" || classificacao ===""  || duracao ==="") {
-        alert("Todos os campos devem ser preenchidos");
-    }
+    let titulo = document.querySelector("#titulo").value;
+    let cod_filme = document.querySelector("#cod_filme").value;
+    let sinopse = document.querySelector("#sinopse").value;
+    let datalanc = document.querySelector("#datalanc").value;
+    let classificacao = document.querySelector("#opcao").value;
+    let duracao = document.querySelector("#duracao").value;
 
-    else{
+    if (titulo === "" || cod_filme === "" || sinopse === "" || datalanc === "" || classificacao === "" || duracao === "") {
+        alert("Todos os campos devem ser preenchidos");
+    } else if (titulo.length < 8) {
+        alert("O título precisa ter pelo menos 8 caracteres");
+    } else {
         alert("Cadastro realizado");
         document.getElementById("titulo").value = "";
         document.getElementById("cod_filme").value = "";
         document.getElementById("sinopse").value = "";
         document.getElementById("datalanc").value = "";
-        document.getElementById("classificacao").value = "";
+        document.getElementById("opcao").value = "";
         document.getElementById("duracao").value = "";
-
     }
 }
